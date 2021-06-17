fe9bd269a2d41da976d883a4dc5ddf18e8a2b58d
Manuel Fideles <manuelfideles77@gmail.com>
2021-06-17 14:23:23 +0100

    Added .env to hide token

--- bot.js
+++ bot.js
@@ -1,1 +1,28 @@
-console.log("I'm alive 🏀");
+// Imports
+const Discord = require('discord.js');
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log("I'm alive 🏀");
+})
+
+client.login(process.env.TOKEN);
+
+/*
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interaction', async interaction => {
+    if (!interaction.isCommand()) return;
+    if (interaction.commandName === 'ping') {
+        await interaction.reply('Pong!');
+    }
+});
+
+client.login('token');
+*/
